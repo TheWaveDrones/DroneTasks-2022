c0c5b5861ac701c4db18c8ad11dd752fb1413225
ShonPortu <81827282+ShonPortu@users.noreply.github.com>
2022-01-29 19:12:43 +0200

    Add files via upload
    Final version for Task 1, with single and continues distance functions
    works 100%, I hope :)

--- android/TheWaveApp/Activities/MainActivity.cs
+++ android/TheWaveApp/Activities/MainActivity.cs
@@ -1,6 +1,7 @@
 ﻿using Android;
 using Android.App;
 using Android.Bluetooth;
+using Android.Content;
 using Android.OS;
 using Android.Runtime;
 using Android.Widget;
@@ -10,6 +11,7 @@ using Java.Util;
 using Model;
 using System;
 using System.Text;
+using TheWaveApp.Services;
 
 namespace TheWaveApp.Activities
 {
@@ -19,16 +21,23 @@ namespace TheWaveApp.Activities
         private string[] perms = new string[] { Manifest.Permission.Bluetooth, Manifest.Permission.BluetoothAdmin, Manifest.Permission.BluetoothPrivileged, Manifest.Permission.AccessFineLocation, Manifest.Permission.AccessCoarseLocation };
         // --- //
 
+        public static byte[] buf;
         private static UUID mUUID = UUID.FromString("00001101-0000-1000-8000-00805F9B34FB");
+        private static int counter = 0;
+        public static OutputStream outputStream;
+        public static InputStream inputStream;
 
         public static BluetoothAdapter bAdapter;
         private BluetoothDevice commDevice;
-        private BluetoothSocket btSocket;
+        public static BluetoothSocket btSocket;
 
         // ----------- //
         private MaterialButton btnConnectionToggle;
         private MaterialButton btnStartRelay;
-        private TextView txtDistance;
+        private MaterialButton btnToggleContinuous;
+        public static TextView txtDistance;
+
+        private object padlock = new object();
 
 
         protected override void OnCreate(Bundle savedInstanceState)
@@ -45,6 +54,7 @@ namespace TheWaveApp.Activities
         {
             txtDistance.Visibility = Android.Views.ViewStates.Invisible;
             btnStartRelay.Visibility = Android.Views.ViewStates.Invisible;
+            btnToggleContinuous.Visibility = Android.Views.ViewStates.Invisible;
         }
 
         private void ConfigureAdapter()
@@ -56,14 +66,15 @@ namespace TheWaveApp.Activities
                 btSocket = commDevice.CreateRfcommSocketToServiceRecord(mUUID);
                 btSocket.Connect();
                 bAdapter.CancelDiscovery();
-
+                outputStream = ((Android.Runtime.OutputStreamInvoker)btSocket.OutputStream).BaseOutputStream;
+                inputStream = ((Android.Runtime.InputStreamInvoker)btSocket.InputStream).BaseInputStream;
             }
             catch (IOException e)
             {
                 Toast.MakeText(this, "Failed to create socket", ToastLength.Short).Show();
                 e.PrintStackTrace();
             }
-
+            
         }
 
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
@@ -80,43 +91,90 @@ namespace TheWaveApp.Activities
 
             btnStartRelay = FindViewById<MaterialButton>(Resource.Id.btnStartRelay);
             btnStartRelay.Click += BtnStartRelay_Click;
+
+            btnToggleContinuous = FindViewById<MaterialButton>(Resource.Id.btnToggleContinuous);
+            btnToggleContinuous.Click += BtnToggleContinuous_Click;
+
             txtDistance = FindViewById<TextView>(Resource.Id.txtDistance);
+            txtDistance.Text = string.Empty;
+        }
+
+        private void BtnToggleContinuous_Click(object sender, EventArgs e)
+        {
+            // if toggled color will be swtiched to green.
+            if(counter == 0)
+            {
+                // stop sending continous data
+
+                btnToggleContinuous.SetBackgroundColor(Android.Graphics.Color.Green);
+                btnToggleContinuous.Text = "Disable Continuous Data Streaming";
+                counter++;
+
+                
+                outputStream.Write((byte)'c');
+                System.Threading.Thread.Sleep(500);
+
+                Intent intent = new Intent(this, typeof(RecieveDataService));
+                StartService(intent);
+
+            }
+            else if(counter == 1)
+            {
+                // now the sensor should send in continous stream
+                btnToggleContinuous.SetBackgroundColor(Android.Graphics.Color.Blue);
+                btnToggleContinuous.Text = "Enable Continuous Data Streaming";
+                counter--;
+                outputStream.Write((byte)'s');
+
+                Intent intent = new Intent(this, typeof(RecieveDataService));
+                StopService(intent);
+            }
         }
 
         private void BtnStartRelay_Click(object sender, EventArgs e)
         {
             // check if char was inputed
 
-            OutputStream outputStream = ((Android.Runtime.OutputStreamInvoker)btSocket.OutputStream).BaseOutputStream;
-            InputStream inputStream = ((Android.Runtime.InputStreamInvoker)btSocket.InputStream).BaseInputStream;
-            try
+            lock (padlock)
             {
-                outputStream.Write((byte)'Z');
-            }
-            catch (IOException ex)
-            {
+                try
+                {
+                    outputStream.Write((byte)'d');
+                    System.Threading.Thread.Sleep(1000);
+                    if (inputStream.Available() > 0)
+                    {
+                        int legnth = inputStream.Available();
+                        buf = new byte[1024];
+                        var nn = inputStream.Read(buf, 0, legnth);
+                        System.Array.Resize(ref buf, nn);
+                        string msg = Encoding.ASCII.GetString(buf);
+                        txtDistance.Visibility = Android.Views.ViewStates.Visible;
+                        txtDistance.Text = msg;
+                    }
+                }
+                catch (IOException ex)
+                {
+                }
             }
 
             // flush stream in case there was residual data left from previous connections
-            inputStream.Skip(inputStream.Available());
+            //inputStream.Skip(inputStream.Available());
 
-            for (int i = 0; i < 100; i++)
-            {
-                byte b = (byte)inputStream.Read();
-                txtDistance.Text = b.ToString();
-                txtDistance.Visibility = Android.Views.ViewStates.Visible;
-            }
 
         }
+
+
 
         private void BtnConnectionToggle_Click(object sender, EventArgs e)
         {
             ConfigureAdapter();
             if (btSocket.IsConnected)
             {
+                outputStream.Write((byte)'s');
                 btnConnectionToggle.Text = $"Connected to\n{commDevice.Name}";
                 btnConnectionToggle.SetBackgroundColor(Android.Graphics.Color.Green);
                 btnStartRelay.Visibility = Android.Views.ViewStates.Visible;
+                btnToggleContinuous.Visibility = Android.Views.ViewStates.Visible;
             }
         }
 
---
+++ android/TheWaveApp/Services/RecieveDataService.cs
@@ -0,0 +1,56 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using TheWaveApp.Activities;
+
+namespace TheWaveApp.Services
+{
+    [Service]
+    public class RecieveDataService : Service
+    {
+        public override IBinder OnBind(Intent intent)
+        {
+            return null;
+        }
+
+        [return: GeneratedEnum]
+        public override StartCommandResult OnStartCommand(Intent intent, [GeneratedEnum] StartCommandFlags flags, int startId)
+        {
+            base.OnStartCommand(intent, flags, startId);
+            Task.Run(() => 
+            {
+                while(true)
+                {
+                    Thread.Sleep(500);
+                    if (MainActivity.inputStream.Available() > 0)
+                    {
+                        int legnth = MainActivity.inputStream.Available();
+                        MainActivity.buf = new byte[1024];
+                        var nn = MainActivity.inputStream.Read(MainActivity.buf, 0, legnth);
+                        System.Array.Resize(ref MainActivity.buf, nn);
+                        string msg = Encoding.ASCII.GetString(MainActivity.buf);
+                        MainActivity.txtDistance.Visibility = Android.Views.ViewStates.Visible;
+                        MainActivity.txtDistance.Text = msg;
+                    }
+                }
+            });
+
+            return StartCommandResult.Sticky;
+        }
+
+        public override void OnDestroy()
+        {
+            // closes the bluetooth connection
+            MainActivity.btSocket.Close();
+        }
+    }
+}
